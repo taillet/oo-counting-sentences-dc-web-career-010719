de4ee50622785a9c0d9b5a1ee528e3df7aed9324
Heloise Taillet <ht2394@columbia.edu>
2019-01-10 03:01:55 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,24 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    if self.include?(".") ||  self.include?("?") || self.include?("!")
+    array = self.split(/[.?!]/).reject{|split| split == ""}
+    array.length
+    else 
+      return 0
+    end
   end
 end
